f8fb8e4dafacc4bfd1d7a51eef3fc7e0b6cc1410
吴少航 <583175694@qq.com>
2020-05-06 12:25:23 +0800

    base

--- example/lib/red_style_page.dart
+++ example/lib/red_style_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_calendar/flutter_custom_calendar.dart';
 
@@ -19,6 +21,15 @@ class _RedStylePageState extends State<RedStylePage> {
   Map<DateModel, String> customExtraData = {};
 
   Color pinkColor = Color(0xffFF8291);
+
+  Map prov = {
+    'value': null,
+    'index': null
+  };
+  Map next = {
+    'value': null,
+    'index': null
+  };
 
   @override
   void initState() {
@@ -48,6 +59,13 @@ class _RedStylePageState extends State<RedStylePage> {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> _list = new List();
+    for (int i = 0; i < titleItems.length; i++) {
+      _list.add(buildListData(context, titleItems[i], iconItems[i], subTitleItems[i], i));
+    }
+    // 分割线
+    var divideTiles = ListTile.divideTiles(context: context, tiles: _list).toList();
+
     var calendarWidget = CalendarViewWidget(
       calendarController: controller,
       margin: EdgeInsets.only(top: 20),
@@ -117,7 +135,15 @@ class _RedStylePageState extends State<RedStylePage> {
                       child: new Text(selectText.value),
                     );
                   }),
-            ],
+            Flexible(
+                child: new ListView.separated(
+                  shrinkWrap: true,
+                  itemBuilder: (context, item) {
+                    return buildListData(context, titleItems[item], iconItems[item], subTitleItems[item], item);
+                    },
+                  separatorBuilder: (BuildContext context, int index) => new Divider(),
+                  itemCount: iconItems.length)
+            )],
           ),
         ),
         floatingActionButton: FloatingActionButton(
@@ -130,6 +156,89 @@ class _RedStylePageState extends State<RedStylePage> {
       ),
     );
   }
+
+  Widget buildListData(BuildContext context, String titleItem, Icon iconItem, String subTitleItem, index) {
+    bool accepted = false;
+
+    return DragTarget(
+      builder: (context, candidateData, rejectedData) {
+        return new ListTile(
+          leading: iconItem,
+          title: new Text(
+            titleItem,
+            style: TextStyle(fontSize: 18),
+          ),
+          subtitle: new Text(
+            subTitleItem,
+          ),
+          trailing: new Draggable(
+            child: new Icon(Icons.adjust),
+            axis: Axis.vertical,
+            feedback: new Container(
+              transform: Matrix4.translationValues(-MediaQuery.of(context).size.width + 50, 0, 0),
+              child: new Text(
+                titleItem,
+                style: TextStyle(fontSize: 25),
+              )
+            ),
+            onDragStarted: () {
+              prov["value"] = titleItem;
+              prov["index"] = index;
+              print(prov);
+            }
+          ),
+        );
+      },
+      //返回是否接收 参数为Draggable的data 方向相同则允许接收
+      onWillAccept: (data) {
+
+        return true;
+      },
+      onAccept: (data) {
+        next["value"] = titleItem;
+        next["index"] = index;
+        accepted = true;
+
+        setState(() {
+          titleItems[next["index"]] = prov["value"];
+          titleItems[prov["index"]] = next["value"];
+        });
+
+        print('prov: $prov, next: $next accepted:$accepted');
+      },
+    );
+  }
+
+  // 数据源
+  List<String> titleItems = <String>[
+    'keyboard', 'print',
+    'router', 'pages',
+    'zoom_out_map', 'zoom_out',
+    'youtube_searched_for', 'wifi_tethering',
+    'wifi_lock', 'widgets',
+    'weekend', 'web',
+    '图accessible', 'ac_unit',
+  ];
+
+  List<Icon> iconItems = <Icon>[
+    new Icon(Icons.keyboard), new Icon(Icons.print),
+    new Icon(Icons.router), new Icon(Icons.pages),
+    new Icon(Icons.zoom_out_map), new Icon(Icons.zoom_out),
+    new Icon(Icons.youtube_searched_for), new Icon(Icons.wifi_tethering),
+    new Icon(Icons.wifi_lock), new Icon(Icons.widgets),
+    new Icon(Icons.weekend), new Icon(Icons.web),
+    new Icon(Icons.accessible), new Icon(Icons.ac_unit),
+  ];
+
+  List<String> subTitleItems = <String>[
+    'subTitle: keyboard', 'subTitle: print',
+    'subTitle: router', 'subTitle: pages',
+    'subTitle: zoom_out_map', 'subTitle: zoom_out',
+    'subTitle: youtube_searched_for', 'subTitle: wifi_tethering',
+    'subTitle: wifi_lock', 'subTitle: widgets',
+    'subTitle: weekend', 'subTitle: web',
+    'subTitle: accessible', 'subTitle: ac_unit',
+  ];
 }
 
 class CustomStyleWeekBarItem extends BaseWeekBar {
